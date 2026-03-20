e1dd8b84ed4f1c3c1953a2bf18080052fb1fd6ee
xtaltia <aletabix@pm.me>
2026-03-20 21:06:45 +0000

    c64Palette as const

--- lib/palettes.ts
+++ lib/palettes.ts
@@ -1,7 +1,7 @@
 import { C } from "./helper";
 
 export const egaPalette = [C.x0, C.x00a, C.x0a0, C.x0aa, C.xa00, C.xa0a, C.xa50, C.xa, C.x5, C.x55f, C.x5f5, C.x5ff, C.xf55, C.xf5f, C.xff5, C.xf] as const;
-export const c64Palette = [C.x0, C.xf, C.x800, C.xafe, C.xc4c, C.x0c5, C.x00a, C.xee7, C.xd85, C.x640, C.xf77, C.x3, C.x7, C.xaf6, C.x08f, C.xb];
+export const c64Palette = [C.x0, C.xf, C.x800, C.xafe, C.xc4c, C.x0c5, C.x00a, C.xee7, C.xd85, C.x640, C.xf77, C.x3, C.x7, C.xaf6, C.x08f, C.xb] as const;
 
 /**
  * Petrichor palette
